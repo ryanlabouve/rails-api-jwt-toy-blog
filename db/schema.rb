7f8591ccb6a51a58f8f957780afd4f092ca2eea7
Ryan LaBouve <imryanlabouve@gmail.com>
2016-04-12 00:59:43 -0500

    Initial spike on user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412054952) do
+ActiveRecord::Schema.define(version: 20160412055926) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20160412054952) do
     t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "password_digest"
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
